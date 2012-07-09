ad1442c190b86235375368ed4e8a718ae5b5d552
abykov <bykov_a_a@mail.ru>
2012-07-09 18:15:45 +0400

    add oid1 and oid2

--- test5.py
+++ test5.py
@@ -1,15 +1,10 @@
 import netsnmp
 from log import *
 
-
-
-oid = netsnmp.VarList(netsnmp.Varbind(OID))
 oid1 = netsnmp.VarList(netsnmp.Varbind(OID1))
-#oid2 = netsnmp.VarList(netsnmp.Varbind(OID2))
-oid_ports = netsnmp.VarList(netsnmp.Varbind(OID_PORTS))
-
-
 res1 = netsnmp.snmpwalk(oid1, Version = 2, DestHost=IP, Community=COMMUNITY)
+
+oid_ports = netsnmp.VarList(netsnmp.Varbind(OID_PORTS))
 res_ports = netsnmp.snmpwalk(oid_ports, Version = 2, DestHost=IP, Community=COMMUNITY)
 
 res_ports=res_ports[9:]
@@ -26,10 +21,22 @@ while i<len(res1)+1:
     A[i][0]=res1[i-1]
     i+=1
 
-
+oid = netsnmp.VarList(netsnmp.Varbind(OID))
 res = netsnmp.snmpwalk(oid, Version = 2, DestHost=IP, Community=COMMUNITY)
 
 for i in range(len(res)):
     w=bin(int(res[i].encode("hex"), 16))[2:]
-    print i,') len',len(w),'  ',w
+    if len(w)<96:
+        w='0'*(96-len(w))+w
+    print w
 
+print '\n=============================================\n'
+
+oid2 = netsnmp.VarList(netsnmp.Varbind(OID2))
+res2 = netsnmp.snmpwalk(oid2, Version = 2, DestHost=IP, Community=COMMUNITY)
+
+for i in range(len(res2)):
+    ww=bin(int(res2[i].encode("hex"), 16))[2:]
+    if len(ww)<96:
+        ww='0'*(96-len(ww))+ww
+    print ww
