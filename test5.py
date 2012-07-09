c427adc4d704f53470a5e2de3de38e9b466d56c8
abykov <bykov_a_a@mail.ru>
2012-07-09 17:39:44 +0400

    log file

--- test5.py
+++ test5.py
@@ -1,46 +1,35 @@
 import netsnmp
+from log import *
 
 
-oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.17.7.1.4.3.1.2'))
-oid1 = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.17.7.1.4.3.1.1'))
-#oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.17.7.1.4.3.1.4'))
-oid2 = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.47.1.1.1.1.7'))
+
+oid = netsnmp.VarList(netsnmp.Varbind(OID))
+oid1 = netsnmp.VarList(netsnmp.Varbind(OID1))
+#oid2 = netsnmp.VarList(netsnmp.Varbind(OID2))
+oid_ports = netsnmp.VarList(netsnmp.Varbind(OID_PORTS))
 
 
-res1 = netsnmp.snmpwalk(oid1, Version = 2, DestHost='172.18.82.20', Community='test2012')
-res2 = netsnmp.snmpwalk(oid2, Version = 2, DestHost='172.18.82.20', Community='test2012')
+res1 = netsnmp.snmpwalk(oid1, Version = 2, DestHost=IP, Community=COMMUNITY)
+res_ports = netsnmp.snmpwalk(oid_ports, Version = 2, DestHost=IP, Community=COMMUNITY)
 
-
-res2=res2[9:]
+res_ports=res_ports[9:]
 
 A = []
 A = [0] * (len(res1)+1)
 for i in range(len(res1)+1):
-    A[i] = [0] * (len(res2)+1)
+    A[i] = [0] * (len(res_ports)+1)
 
-A[0][1:]=res2
+A[0][1:]=res_ports
 
 i=1
 while i<len(res1)+1:
     A[i][0]=res1[i-1]
     i+=1
 
-#for i in range(len(res1)+1):
-#        print A[i]
 
-res = netsnmp.snmpwalk(oid, Version = 2, DestHost='172.18.82.20', Community='test2012')
-print res
+res = netsnmp.snmpwalk(oid, Version = 2, DestHost=IP, Community=COMMUNITY)
 
 for i in range(len(res)):
-    #print bin(int(res[i], 16))[2:]
-    print 'str',res[i]
-
-s=str(res[1])
-print 'len ',len(s), 's1', s[1]
-#s.replace(' ' ,'')
-
-
-#print s
-
-
+    w=bin(int(res[i].encode("hex"), 16))[2:]
+    print i,') len',len(w),'  ',w
 
