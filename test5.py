b0b0819edbd65e4d0d02c2ecedd66d0f4d10f162
abykov <bykov_a_a@mail.ru>
2012-07-10 15:30:45 +0400

    number of vlan

--- test5.py
+++ test5.py
@@ -1,5 +1,23 @@
 import netsnmp
+import os
 from log import *
+
+os.system('snmpwalk -v2c -c test2012 172.18.82.20 .1.3.6.1.2.1.17.7.1.4.3.1.1 > qwe.dat')
+file=open('qwe.dat')
+N=len(file.readlines())
+
+
+def number_of_vlan(j):
+    i=0
+    A=[]
+    file=open('qwe.dat')
+    while i<N:
+        x=file.readline()
+        s=x.split(' ')
+        A.append(s[0][29:])
+        i+=1
+    return A[j]
+
 
 def RES(OID):
     oid = netsnmp.VarList(netsnmp.Varbind(OID))
@@ -22,8 +40,13 @@ A[0][1:]=res_ports
 
 i=1
 while i<len(res1)+1:
-    A[i][0]=res1[i-1]
+    A[i][0]=res1[i-1]+' '+number_of_vlan(i-1)
     i+=1
+
+
+
+for i in range(len(res1)+1):
+    print A[i]
 
 res=RES(OID)
 
@@ -42,6 +65,7 @@ for i in range(len(res2)):
     if len(ww)<96:
         ww='0'*(96-len(ww))+ww
     print ww
+
 
 #for i in range(len(res_ports)):
 #    print ww[i*2],' ',ww[i*2+1]
