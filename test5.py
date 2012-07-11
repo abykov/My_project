c77246020fc10dea9fa8de4eceaaa8d4040a5662
abykov <bykov_a_a@mail.ru>
2012-07-11 14:27:52 +0400

    O and X, center

--- test5.py
+++ test5.py
@@ -15,7 +15,11 @@ def HTML(b,M,N):
     f_out.write('<html><head><title>Table </title></head><body><p>Table.</p><table border="1"><tr>\n')
     for i in range(N):
         for j in range(M):
-            f_out.write('<td>'+b[j][i]+'</td>\n')
+            if b[j][i]=='1':
+                d='X'
+            elif b[j][i]=='0':d="O"
+            else:d=b[j][i]
+            f_out.write('<td align="center">'+d+'</td>\n')
         f_out.write('</tr><tr>\n')
     f_out.write('</tr></table></body></html>')
     f_out.close()
@@ -66,7 +70,9 @@ for i in range(len(res)):
     if len(w)<96:
         w='0'*(96-len(w))+w
     A[i+1][1:]=w[:48]
+    print w
 
+print '\nsssssssssssssssssssssssssssss\n'
 
 res2 = RES(OID2)
 
@@ -74,6 +80,7 @@ for i in range(len(res2)):
     ww=bin(int(res2[i].encode("hex"), 16))[2:]
     if len(ww)<96:
         ww='0'*(96-len(ww))+ww
+    print ww
 
 
 HTML(A,len(res1)+1,len(res_ports)+1)
