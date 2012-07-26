4d61016db44b853cc7bd15b8f96333c01f0df26a
abykov <bykov_a_a@mail.ru>
2012-07-26 12:52:09 +0400

    123

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import netsnmp
 import xmlrpclib
 import datetime
@@ -10,7 +11,7 @@ def Report_on_switch(COMMUNITY, IP):
 
     Use this function to creates a table of vlans and ports for switches. The main function.
     '''
-    string = snmpw + ' ' + COMMUNITY + ' ' + IP + ' ' + OID_VLANS_NAMES + ' -On > ' + out_file
+    string = snmpw[0] + ' ' + COMMUNITY + ' ' + IP + ' ' + OID_VLANS_NAMES +' '+snmpw[1] + ' ' + out_file
     os.system(string)
 
     try:
@@ -50,9 +51,9 @@ def Report_on_switch(COMMUNITY, IP):
         return res
 
 
-    def number_of_vlan(number):
+    def number_of_vlan():
         '''
-        number_of_vlan(number)
+        number_of_vlan()
 
         This function returns the number of vlan.
         '''
@@ -64,57 +65,89 @@ def Report_on_switch(COMMUNITY, IP):
             s = x.split(' ')
             my_array.append(s[0][28:]) #[28:] - 29 item numbers begin number of vlan
             i += 1
-        return my_array[number]
+        return my_array
 
 
-    def HTML(array_to_html, width_array, length_array, title):
+    def ports_description():
         '''
-        HTML(array_to_html, width_array, length_array, title)
+        ports_description()
+
+        This function returns the description of port.
+        '''
+        list_ports_description = RES(OID_PORTS_DESCRIPTION, 'walk')
+        return list_ports_description
+
+
+    def HTML(array_to_html, width_array, length_array, title, number_of_vlan_array, ports_description_array):
+        '''
+        HTML(array_to_html, width_array, length_array, title, ports_description_array)
 
         This function creates a html page with a table.
         '''
         html_file = title + '.html'
         file_out = open(html_file, 'w')
         file_out.write(
-            '<html><head><title>' + title +\
-            '</title><style type="text/css">TABLE{background: #fffff0;}TR.even {background: #fffacd;}</style>'
-            '</head><body><p align = "center"><b >' + title + ' (' + str(datetime.datetime.now())[:-7] + ')' +\
+            '<html><head><title>' + title +'</title>'
+            '<style type="text/css">TABLE{background: #fffff0;border: 2px solid #808080;}TR.even {background: #fffacd;}'
+            '</style></head><body><p align = "center"><b >' + title + ' (' + str(datetime.datetime.now())[:-7] + ')' +\
             '</b></p><table  CELLPADDING = 4 CELLSPACING = 0 border = "1"><tr class="even">\n')
+
         for i in range(length_array):
+            if i == 1:
+                for j in range(width_array + 1):
+                    if j == 0: char = ' '
+                    elif j == 1: char = 'Ports description '
+                    else: char = '# ' + number_of_vlan_array[j - 2]
+                    file_out.write('<td  align = "center">' + char + '</td>\n')
+                file_out.write('</tr><tr >\n')
             for j in range(width_array):
+                if j == 1:
+                    char = str(ports_description_array[i - 1])
+                    if i == 0: char = ' '
+                    file_out.write('<td  align = "center">' + char + '</td>\n')
                 if array_to_html[j][i] == '1': char = 'T'
                 elif array_to_html[j][i] == '2': char = 'U'
-                elif array_to_html[j][i] == '0': char = "-"
+                elif array_to_html[j][i] == '0': char = '-'
                 else: char = array_to_html[j][i]
                 file_out.write('<td  align = "center">' + char + '</td>\n')
-            if i % 2 == 1: file_out.write('</tr><tr class="even">\n')
+            if (i % 2 == 1) or (i == 0): file_out.write('</tr><tr class="even">\n')
             else: file_out.write('</tr><tr >\n')
-
         file_out.write('</tr></table></body></html>')
         file_out.close()
 
 
-    def WIKI(array_to_wiki, width_array, length_array, title, WikiToken, Wiki):
+    def WIKI(array_to_wiki, width_array, length_array, title, WikiToken, Wiki, number_of_vlan_array, ports_description_array):
         '''
-        WIKI(array_to_wiki, width_array, length_array, title, WikiToken, Wiki)
+        WIKI(array_to_wiki, width_array, length_array, title, WikiToken, Wiki, ports_description_array)
 
         This function sends the table to the specified wiki page
         and create a new page, if this page does not exist.
         '''
         content = '{wiki}\n'
         for i in range(length_array):
+            if i == 1:
+                for j in range(width_array + 1):
+                    if j == 0: char = ' '
+                    elif j == 1: char = 'Ports description '
+                    else: char = '№ ' + number_of_vlan_array[j - 2]
+                    content += '||' + char
+                content += '|\n'
             for j in range(width_array):
+                if j == 1:
+                    char = str(ports_description_array[i - 1])
+                    if i == 0: char = ' '
+                    content += '||' + char
                 if i == 0:
                     content += '||' + array_to_wiki[j][i]
                 else:
                     if array_to_wiki[j][i] == '1': char = 'T'
                     elif array_to_wiki[j][i] == '2': char = 'U'
-                    elif array_to_wiki[j][i] == '0': char = "-"
+                    elif array_to_wiki[j][i] == '0': char = '-'
                     else: char = array_to_wiki[j][i]
-                    content += '|' + char
+                    if j == 0: content += '||' + char
+                    else: content += '|' + char
             content += '|\n'
         content += '{wiki}'
-
         try:
             page = Wiki.confluence2.getPage(WikiToken, SPACE, title)
             page['content'] = ' '
@@ -159,15 +192,15 @@ def Report_on_switch(COMMUNITY, IP):
 
     list_ports = tuple_ports
 
-    vlans_ports = ['0'] * (len(list_vlans_names) + 1)
+    vlans_ports = [' '] * (len(list_vlans_names) + 1)
     for i in range(len(list_vlans_names) + 1):
-        vlans_ports[i] = ['0'] * (len(list_ports) + 1)
+        vlans_ports[i] = [' '] * (len(list_ports) + 1)
 
     vlans_ports[0][1:] = list_ports
 
     i = 1
     while i < len(list_vlans_names) + 1:
-        vlans_ports[i][0] = list_vlans_names[i - 1] + ' ' + number_of_vlan(i - 1)
+        vlans_ports[i][0] = list_vlans_names[i - 1]
         i += 1
 
     list_tag_vlans = RES(OID_TAG_PORTS, 'walk')
@@ -204,9 +237,10 @@ def Report_on_switch(COMMUNITY, IP):
         i += 1
 
     if OUTPUT_TO == 'wiki':
-        WIKI(vlans_ports, len(list_vlans_names) + 1, len(list_ports) + 1, title, WikiToken, Wiki)
+        WIKI(vlans_ports, len(list_vlans_names) + 1, len(list_ports) + 1, title, WikiToken, Wiki, number_of_vlan(),
+            ports_description())
     else:
-        HTML(vlans_ports, len(list_vlans_names) + 1, len(list_ports) + 1, title)
+        HTML(vlans_ports, len(list_vlans_names) + 1, len(list_ports) + 1, title, number_of_vlan(), ports_description())
 
 
 for i in range(len(switches)):
